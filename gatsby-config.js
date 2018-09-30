7a95bacb69f744360f758350bc4b27509814c20f
Sathish Paul Leo <sathishpaul@gmail.com>
2018-09-29 18:42:02 -0700

    Updated site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'sathishpaul.com',
+        short_name: 'My website',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
